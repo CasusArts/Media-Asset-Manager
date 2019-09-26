817cdd6ebb3cd222ec91884a8804199707b65060
Fiodor Gorobet <casusarts@gmail.com>
2019-09-26 15:59:42 +0200

    Revert "add tracks and index templates, add navbar, add urlpattern"
    This reverts commit 55da6a3f72b8ec286d622825cb24744668df0ae3.

--- mam/manager/urls.py
+++ mam/manager/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'manager'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('tracks/', views.all_tracks, name='tracks'),
 ]
--- mam/manager/views.py
+++ mam/manager/views.py
@@ -1,18 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Track, Artist, Label, Album
 
 
 def index(request):
-    return render(request, 'manager/index.html')
-
-
-def all_tracks(request):
     all_tracks = Track.objects.order_by('track_year')
     context = {
         'all_tracks': all_tracks,
     }
-    return render(request, 'manager/tracks.html', context)
-
-
+    return render(request, 'manager/index.html', context)
