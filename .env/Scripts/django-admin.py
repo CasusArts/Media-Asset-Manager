ce2d08514bb1fc3f9dc79365f08477283e79c3a2
Fiodor Gorobet <casusarts@gmail.com>
2019-09-26 15:58:18 +0200

    Failed 4

---
+++ .env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\casus\development\py_dev\media_asset_manager\.env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
