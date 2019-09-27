223956281520091f05eecd72a4860047afac748b
Fiodor Gorobet <casusarts@gmail.com>
2019-09-27 11:32:41 +0200

    refactor navbar active link

--- mam/manager/views.py
+++ mam/manager/views.py
@@ -4,12 +4,14 @@ from .models import Track, Artist, Label, Album
 
 
 def index(request):
-    return render(request, 'manager/index.html')
+    context = {'home_page': 'active'}
+    return render(request, 'manager/index.html', context)
 
 
 def list_all_tracks(request):
     all_tracks = Track.objects.order_by('track_year')
     context = {
         'all_tracks': all_tracks,
+        'tracks_page': 'active'
     }
     return render(request, 'manager/tracks.html', context)
